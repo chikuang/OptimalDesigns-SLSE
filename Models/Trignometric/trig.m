ef3d89ae456c90991f03204df03c147967aa1a12
ChiKuang <chikuang@uvic.ca>
2017-11-15 11:58:42 -0800

    Add trignometric model

---
+++ Models/Trignometric/trig.m
@@ -0,0 +1,14 @@
+% Author : Chi-Kuang Yeh 
+% Date: 2017 Nov 15
+
+% Purpose: To calculate the gradient of the function in
+%   trignometric model of order n
+function ANS =trig(xi,theta)
+  q2 = length(theta); %the order of the polynomial
+  ANS = zeros(q2,1);
+  h = q2/2;
+  for i = 1:q2/2
+    ANS(i,1) = cos(i*xi);   % first half is all cos
+    ANS(i+h,1) = sin(i*xi); % second half is all sin
+  end
+end
