e1b426c8f1dc0be7238b91e7abdba9936c228518
ChiKuang <chikuang@uvic.ca>
2017-11-15 12:17:01 -0800

    minor modification

--- D_opt_mod.m
+++ D_opt_mod.m
@@ -14,7 +14,7 @@ function [del , ANS, error] = D_opt_mod(N,t,theta,range,fun)
   sqt = sqrt(t);
   
   %% cvx part
-  cvx_begin quiet
+  cvx_begin
     cvx_precision high
     variables w(N,1) del(1)%design variable and upper bound
     minimize del(1)
