ba531f18bac4e335a49dfa385f09ee929bf5ae74
ChiKuang <chikuang@uvic.ca>
2017-11-19 13:22:37 -0800

    tirg & poly, vectorize

--- Models/Polynomial/poly_mat/ploy_mat.m
+++ Models/Polynomial/poly_mat/ploy_mat.m
@@ -3,7 +3,7 @@
 
 % Purpose: To calculate the gradient of the function in
 %   polynomial model of order n
-function ANS =poly(xi,theta)
+function ANS = poly(xi,theta)
    ANS = zeros(length(theta), length(xi));
   for i = 1 : length(xi)
     ANS(:,i) = xi(i).^(1:length(theta));
