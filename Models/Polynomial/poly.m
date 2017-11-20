1c2b1125833d8ad6b23c7f965464078f207310f4
chikuang <chikuang@uvic.ca>
2017-11-19 23:25:28 -0800

    quiet, precision change

--- Models/Polynomial/poly.m
+++ Models/Polynomial/poly.m
@@ -4,11 +4,6 @@
 % Purpose: To calculate the gradient of the function in
 %   polynomial model of order n
 function ANS =poly(xi,theta)
-  %n = length(theta); %the order of the polynomial
-  %ANS = zeros(n,1);
-  %for i = 1:n
-  %  ANS(i,1) = xi.^i;
-  %end
   ANS = zeros(length(theta),1);
   ANS(:) = xi.^(1:length(theta));
 end
