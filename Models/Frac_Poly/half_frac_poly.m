7dc01d480c811e0658011e5325437b7ce0e96e5a
ChiKuang <chikuang@uvic.ca>
2018-01-17 20:05:02 -0800

    Modify - Half fractional

--- Models/Frac_Poly/half_frac_poly.m
+++ Models/Frac_Poly/half_frac_poly.m
@@ -4,9 +4,6 @@
 % Purpose: To calculate the gradient of the function in
 %   fractional polynomial model of order n
 function ANS =half_frac_poly(xi,theta)
-  n = length(theta); 
-  ANS = zeros(n,1);
-  for i = 1:n
-    ANS(i,1) = (xi^(1/2)).^i;
-  end
+  ANS = zeros(length(theta),1);
+  ANS(:) = (xi^(1/2)).^(1:length(theta));
 end
