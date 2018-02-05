5f82c23f5c463fa6e39931159f3883ea6a6defda
Chi-Kuang Yeh <chikuang@uvic.ca>
2018-02-05 14:05:13 -0800

    Gom, add plot points

--- plot/gom_plot.m
+++ plot/gom_plot.m
@@ -5,10 +5,13 @@ figure
   h1 = plot(x,y);
   xlabel('design space (S)','FontSize', 16); % x-axis label
   ylabel('g(x;\theta)','FontSize', 16); % y-axis label
+  xlim([-0.05,5.5]);
   ylim([-0.05,1.5]);
 hold on
-  x1 = [0.0650;0.5800;5.0000];
-  h2 = plot(x1,[0;0;0,],'*','Color','Red');
+  x1 = [0.0645;0.5790;5.0000];
+  y1 = [0;0;0];
+  h2 = plot(x1,y1,'*','Color','Red');
   lgd = legend([h1 h2], 'g(x;\theta)','Support points');
   lgd.FontSize = 12;
+  text(x1-0.08,y1+0.1,'$\frac{1}{3}$','Interpreter','latex','FontSize',14);
 hold off 
