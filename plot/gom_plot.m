db51b23bac1c045316fa6d2cdae688b4e7b2dd46
Chi-Kuang Yeh <chikuang@uvic.ca>
2018-03-19 18:28:34 -0700

    A little modify

--- plot/gom_plot.m
+++ plot/gom_plot.m
@@ -3,7 +3,7 @@ f = @(x) exp(-2*exp(-3*x));
 y = f(x);
 figure
   h1 = plot(x,y);
-  xlabel('design space (S)','FontSize', 16); % x-axis label
+  xlabel('design space ( S=[0,5] )','FontSize', 16); % x-axis label
   ylabel('g(x;\theta)','FontSize', 16); % y-axis label
   xlim([-0.05,5.5]);
   ylim([-0.05,1.5]);
@@ -12,6 +12,6 @@ hold on
   y1 = [0;0;0];
   h2 = plot(x1,y1,'*','Color','Red');
   lgd = legend([h1 h2], 'g(x;\theta)','Support points');
-  lgd.FontSize = 12;
-  text(x1-0.08,y1+0.1,'$\frac{1}{3}$','Interpreter','latex','FontSize',14);
+  lgd.FontSize = 16;
+  text(x1,y1+0.05,'$\frac{1}{3}$','Interpreter','latex','FontSize',16);
 hold off 
