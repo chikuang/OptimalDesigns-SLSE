0e2618723ac05ad992606b9f5133efebd3c01da7
ChiKuang <chikuang@uvic.ca>
2018-03-27 19:31:56 -0700

    change the complexity number

--- emax_complexity_N.m
+++ emax_complexity_N.m
@@ -84,11 +84,15 @@ tic
 toc
 
 tic
-  display('N is 500001')
-  [d , a, e] = D_opt_mod(500001,0,[1;2;1],[1e-5;10],@emax);
+  display('N is 300001')
+  [d , a, e] = D_opt_mod(300001,0,[1;2;1],[1e-5;10],@emax);
 toc
 
 tic
-  display('N is 1000001')
-  [d , a, e] = D_opt_mod(5000001,0,[1;2;1],[1e-5;10],@emax);
+  display('N is 400001')
+  [d , a, e] = D_opt_mod(400001,0,[1;2;1],[1e-5;10],@emax);
+toc
+tic
+  display('N is 500001')
+  [d , a, e] = D_opt_mod(500001,0,[1;2;1],[1e-5;10],@emax);
 toc
