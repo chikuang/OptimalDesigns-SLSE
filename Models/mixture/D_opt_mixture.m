0dfc61eb479239d245543bd89004e27a621d65bb
chikuang <chikuang@uvic.ca>
2018-04-01 21:46:27 -0700

    Model: Mixture
    Complete

--- Models/mixture/D_opt_mixture.m
+++ Models/mixture/D_opt_mixture.m
@@ -4,32 +4,45 @@
 % Optimal designs for regression models using 2nd order LSE
 
 %% function itself
-function [del , ANS, error] = D_opt_mixture(N1,N2,N3,t,theta,range,fun)    
+function [del , ANS, error,pt] = D_opt_mixture(N1,N2,N3,t,theta,range,fun)    
   %% initialization
   
+  counter = 0;
   % these ones are used in solving the question
   u1 = range(1)+(range(2)-range(1))*((1:N1)-1)/(N1-1); %discretized equally spaced space
   u2 = range(1)+(range(2)-range(1))*((1:N2)-1)/(N2-1); %discretized equally spaced space
   u3 = range(1)+(range(2)-range(1))*((1:N3)-1)/(N3-1); %discretized equally spaced space
-  N=N1*N2*N3;
-  w = zeros(N,1); n = length(theta); del = 0 ; one_vec = ones(N,1);zero_vec = zeros(N,1);
+  for i = 1:N1
+    for j = 1:N2
+      for k = 1:N3
+        if (u1(i)+u2(j)+u3(k)<=1)
+          x(counter+1,:) = [u1(i);u2(j);u3(k)];
+          counter = counter + 1;
+        end
+      end
+    end
+  end    
+  pt = length(x);
+  n = 8;%length(theta);
+  w = zeros(length(x),1); del = 0 ; one_vec = ones(length(x),1);
+  zero_vec = zeros(length(x),1);
   g1 = zeros(n,1); G2 = zeros(n);
   sqt = sqrt(t);
   
   %% cvx part
   cvx_begin quiet
     cvx_precision best
-    variables w(N,1) del(1)%design variable and upper bound
+    variables w(length(x),1) del(1)%design variable and upper bound
     minimize del(1)
     subject to
 %    constructing the B matrix
-     for i = 1:N
-       f = fun(u(i),theta);
+     for i = 1:length(x)
+       %f = fun(x,theta);
+       f = fun(x(i,:),theta);%[x(i,1);x(i,2);x(i,3);x(i,1)^2;x(i,2)^2;x(i,3)^2;x(i,1)*x(i,2);x(i,1)*x(i,3)];
        g1 = g1 + w(i)*f;
        G2 = G2 + w(i)*f*f';
      end
      B = [1 sqt*g1' ; sqt*g1 G2];
-
       % constrains
       -log_det(B) <= del;
       %-w <= zeros(length(w),1);
@@ -39,61 +52,61 @@ function [del , ANS, error] = D_opt_mixture(N1,N2,N3,t,theta,range,fun)
   
   %% propose the outputs
   kk = find(w>1e-4); % because the computer does not have exact zero
-  ANS = [u(kk);w(kk)']; % return the answer
+  ANS = [x(kk,:)';w(kk)']; % return the answer
 
   % prepare the variables
    %BI = inv(B);
    
    %BI = 1/B;
-   phi_D = zeros(N,1);
-  for i = 1:N
-    f = fun(u(i),theta);
+   phi_D = zeros(length(x),1);
+  for i = 1:length(x)
+    f = fun(x(i,:),theta);%[x(i,1);x(i,2);x(i,3);x(i,1)^2;x(i,2)^2;x(i,3)^2;x(i,1)*x(i,2);x(i,1)*x(i,3)];
     I = [1 sqt*f' ; sqt*f  f*f'];
     %phi_D(i) = trace( BI*I) ;
     phi_D(i) = trace(B\I);
   end
-  q = (n+1)*ones(N,1);
+  q = (n+1)*ones(length(x),1);
   
   % update the error
   error = max(phi_D - q) ;
   
-  %% plot
-  % first, we increase the graphing domain
-  new_range = [0;0]; add_dist = (range(2)-range(1)) /20;
-  new_range(1) = range(1) - add_dist;
-  new_range(2) = range(2) + add_dist;
-  
-  % first plot, plot out the design points
-  figure
-    stem( u,w,'kd');
-    xlim(new_range);  % increase the domain, so the points wont be on the edge
-    ylim([0,1]);
-    xlabel('design space','FontSize', 16) % x-axis label
-    ylabel('weights','FontSize', 16) % y-axis label
-    title('Discretized weight distribution','FontSize', 20)
-  
-  % directional derivative plot
-  fx = @(x) fun(x,theta);
-  ff = @(x) trace(B\[1 sqt.*fx(x)' ; sqt.*fx(x)  fx(x)*fx(x)'] ) - (n+1);
-  %ff = @(x) trace(BI* [1 sqrt(t)*fx(x)' ; sqrt(t)*fx(x)  fx(x)*fx(x)'] ) - (n+1);
-  
-  mini = min(phi_D-q);
-  figure
-    h1 = plot(u,phi_D-q,'+'); %discretized
-    xlim(new_range);
-    ylim([mini+mini/10,1]);
-  hold on
-    y = zeros(size(u));
-    for i =  1:length(u)
-      y(i) = ff(u(i));
-    end
-    h2 = plot(u,y,'-'); %function
-  hold on
-    line(new_range,[0,0],'Color','blue','LineStyle','--');
-  hold on
-    h3 = plot(u(kk),zeros(1,length(kk)),'pg'); %supporting points
-    legend([h1 h2 h3], 'Discretized','d(x,\theta)','Supporting point');
-    xlabel('design space','FontSize', 16) % x-axis label
-    ylabel('Directional Derivative','FontSize', 16) % y-axis label
-  hold off
+%   % plot
+%   first, we increase the graphing domain
+%   new_range = [0;0]; add_dist = (range(2)-range(1)) /20;
+%   new_range(1) = range(1) - add_dist;
+%   new_range(2) = range(2) + add_dist;
+%   
+%   first plot, plot out the design points
+%   figure
+%     stem( u,w,'kd');
+%     xlim(new_range);  % increase the domain, so the points wont be on the edge
+%     ylim([0,1]);
+%     xlabel('design space','FontSize', 16) % x-axis label
+%     ylabel('weights','FontSize', 16) % y-axis label
+%     title('Discretized weight distribution','FontSize', 20)
+%   
+%   directional derivative plot
+%   fx = @(x) fun(x,theta);
+%   ff = @(x) trace(B\[1 sqt.*fx(x)' ; sqt.*fx(x)  fx(x)*fx(x)'] ) - (n+1);
+%   ff = @(x) trace(BI* [1 sqrt(t)*fx(x)' ; sqrt(t)*fx(x)  fx(x)*fx(x)'] ) - (n+1);
+%   
+%   mini = min(phi_D-q);
+%   figure
+%     h1 = plot(u,phi_D-q,'+'); %discretized
+%     xlim(new_range);
+%     ylim([mini+mini/10,1]);
+%   hold on
+%     y = zeros(size(u));
+%     for i =  1:length(u)
+%       y(i) = ff(u(i));
+%     end
+%     h2 = plot(u,y,'-'); %function
+%   hold on
+%     line(new_range,[0,0],'Color','blue','LineStyle','--');
+%   hold on
+%     h3 = plot(u(kk),zeros(1,length(kk)),'pg'); %supporting points
+%     legend([h1 h2 h3], 'Discretized','d(x,\theta)','Supporting point');
+%     xlabel('design space','FontSize', 16) % x-axis label
+%     ylabel('Directional Derivative','FontSize', 16) % y-axis label
+%   hold off
 end
